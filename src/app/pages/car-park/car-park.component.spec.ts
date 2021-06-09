d7dccdda1de49ece1342163743ac6a33324e1932
MDELUCIO <midlelucio@gmail.com>
2021-06-09 03:11:07 +0200

    AppComponent & CarParkComponent
    Testing

--- src/app/pages/car-park/car-park.component.spec.ts
+++ src/app/pages/car-park/car-park.component.spec.ts
@@ -1,5 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MatDialogModule } from '@angular/material/dialog';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ToastrModule } from 'ngx-toastr';
+import { ParkingServiceModule } from 'src/app/shared/services/parking/parking.service.module';
 import { CarParkComponent } from './car-park.component';
 
 describe('CarParkComponent', () => {
@@ -8,6 +11,11 @@ describe('CarParkComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ RouterTestingModule, ParkingServiceModule, 
+        ToastrModule.forRoot({
+          positionClass :'toast-bottom-right'
+        }),
+        MatDialogModule ],
       declarations: [ CarParkComponent ]
     })
     .compileComponents();
